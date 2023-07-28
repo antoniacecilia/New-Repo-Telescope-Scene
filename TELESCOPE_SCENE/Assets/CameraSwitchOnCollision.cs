6a48739d68cde5017c77535bc89853ee3a4815ce
Antonia Cecilia <126273091+antoniacecilia@users.noreply.github.com>
2023-07-28 10:53:52 +0200

    Audio and small fix

--- TELESCOPE_SCENE/Assets/CameraSwitchOnCollision.cs
+++ TELESCOPE_SCENE/Assets/CameraSwitchOnCollision.cs
@@ -1,20 +1,61 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.XR;
+
 public class CameraSwitchOnCollision : MonoBehaviour
 {
-    public Camera mainCamera; // Assign your main camera to this field in the Inspector
-    public Camera alternateCamera; // Assign the camera you want to switch to in the Inspector
-    private bool hasSwitched = false;
-    private void OnTriggerEnter(Collider other)
+    public Camera mainCamera;
+    public Camera alternateCamera;
+    public bool hasSwitched = false;
+    public float panSpeed = 1.0f;
+    public float maxRotation = 45f;
+    private float currentRotation = 0f;
+    public float switchBackDelay = 5f; // Added this line to define the delay
+
+    private void Update()
     {
-        if (!hasSwitched && other.CompareTag("Player")) // Adjust the tag based on your XR rig or collider's tag
+        InputDevice device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+
+        if (hasSwitched)
         {
-            SwitchCamera();
+            float horizontalInput = primary2DAxisValue.x;
+            float desiredRotation = currentRotation + horizontalInput * panSpeed;
+            desiredRotation = Mathf.Clamp(desiredRotation, -maxRotation, maxRotation);
+            float rotationThisFrame = desiredRotation - currentRotation;
+
+            alternateCamera.transform.Rotate(0, rotationThisFrame, 0);
+            currentRotation = desiredRotation;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasSwitched && other.CompareTag("Player"))
+        {
+            SwitchCamera();
+            StartCoroutine(SwitchBackAfterDelay());
+        }
+    }
+
     private void SwitchCamera()
     {
         mainCamera.enabled = false;
         alternateCamera.enabled = true;
         hasSwitched = true;
+    }
+
+    private IEnumerator SwitchBackAfterDelay()
+    {
+        yield return new WaitForSeconds(switchBackDelay); // Modified this line to use the switchBackDelay variable
+        SwitchToMainCamera();
+    }
+
+    private void SwitchToMainCamera()
+    {
+        mainCamera.enabled = true;
+        alternateCamera.enabled = false;
+        hasSwitched = false;
+        currentRotation = 0;
     }
 }
