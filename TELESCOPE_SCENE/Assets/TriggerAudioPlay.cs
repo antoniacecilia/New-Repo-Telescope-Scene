eaa15bce5992a5299fae8fcfbd958a89ec8bb133
Antonia Cecilia <126273091+antoniacecilia@users.noreply.github.com>
2023-07-29 17:02:18 +0200

    Added UI
    And poem

---
+++ TELESCOPE_SCENE/Assets/TriggerAudioPlay.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TriggerAudioPlay : MonoBehaviour
+{
+    public AudioSource audioSource; // Assign your AudioSource in the Inspector
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Adjust this tag based on your player's tag
+        {
+            audioSource.Play();
+        }
+    }
+}
